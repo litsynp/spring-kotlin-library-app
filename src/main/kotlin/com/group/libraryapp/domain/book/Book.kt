821aa3b8dd2952afca88786ff39036a06badc319
litsynp <nocte55is@gmail.com>
2022-08-10 16:45:45 +0900

    refactor: convert Book from Java to Kotlin

---
+++ src/main/kotlin/com/group/libraryapp/domain/book/Book.kt
@@ -0,0 +1,24 @@
+package com.group.libraryapp.domain.book
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+class Book(
+    val name: String,
+
+    // 디폴트 파라미터는 관례상 가장 밑에 위치
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+) {
+
+    init {
+        if (name.isBlank()) {
+            throw IllegalArgumentException("이름은 비어 있을 수 없습니다")
+        }
+    }
+
+}
