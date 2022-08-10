4f2223a8bffa1405d0cc4538b4fb06a488ded0b8
litsynp <nocte55is@gmail.com>
2022-08-10 23:07:28 +0900

    refactor: convert UserService from Java to Kotlin

---
+++ src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
@@ -0,0 +1,43 @@
+package com.group.libraryapp.service.user
+
+import com.group.libraryapp.domain.user.User
+import com.group.libraryapp.domain.user.UserRepository
+import com.group.libraryapp.dto.user.request.UserCreateRequest
+import com.group.libraryapp.dto.user.request.UserUpdateRequest
+import com.group.libraryapp.dto.user.response.UserResponse
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import kotlin.streams.toList
+
+@Service
+class UserService(
+    private val userRepository: UserRepository,
+) {
+
+    @Transactional
+    fun saveUser(request: UserCreateRequest) {
+        val newUser = User(request.name, request.age)
+        userRepository.save(newUser)
+    }
+
+    @Transactional(readOnly = true)
+    fun getUsers(): List<UserResponse> {
+        return userRepository.findAll()
+            .map { user -> UserResponse(user) }
+        // .map { UserResponse(it) } // it 으로 참조
+        // .map(::UserResponse) // 생성자 호출 (Java: UserResponse::new)
+    }
+
+    @Transactional
+    fun updateUserName(request: UserUpdateRequest) {
+        val user = userRepository.findById(request.id).orElseThrow(::IllegalArgumentException)
+        user.updateName(request.name)
+    }
+
+    @Transactional
+    fun deleteUser(name: String) {
+        val user = userRepository.findByName(name).orElseThrow(::IllegalArgumentException)
+        userRepository.delete(user)
+    }
+
+}
